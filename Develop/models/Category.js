2fbec9981ba1b0ed955507042d41f9f0698af415
Josh Grube <steamjgrube@gmail.com>
2021-06-13 21:19:12 -0400

    final

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -6,8 +6,19 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
-  },
+    // define an id column
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    // define category_name column
+    category_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+},
   {
     sequelize,
     timestamps: false,
--- Develop/seeds/product-seeds.js
+++ Develop/seeds/product-seeds.js
@@ -2,31 +2,31 @@ const { Product } = require('../models');
 
 const productData = [
   {
-    product_name: 'Plain T-Shirt',
+    product_name: 'Pack of socks',
     price: 14.99,
     stock: 14,
     category_id: 1,
   },
   {
-    product_name: 'Running Sneakers',
+    product_name: 'Joggers',
     price: 90.0,
     stock: 25,
     category_id: 5,
   },
   {
-    product_name: 'Branded Baseball Hat',
+    product_name: 'MLB issue glove',
     price: 22.99,
     stock: 12,
     category_id: 4,
   },
   {
-    product_name: 'Top 40 Music Compilation Vinyl Record',
+    product_name: 'The Beatles Vinyl Record',
     price: 12.99,
     stock: 50,
     category_id: 3,
   },
   {
-    product_name: 'Cargo Shorts',
+    product_name: 'Cargo pants',
     price: 29.99,
     stock: 22,
     category_id: 2,
